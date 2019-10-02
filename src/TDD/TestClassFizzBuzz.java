5de30e558588c8c5322b8ecfef7e5fc27395e252
rattiyakorn <rattiyakornl@kkumail.com>
2019-10-02 16:54:09 +0700

    part1Byjoy

---
+++ src/TDD/TestClassFizzBuzz.java
@@ -0,0 +1,15 @@
+package TDD;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestClassFizzBuzz {
+
+	@Test
+	void input_is_1_expect_1() {
+		int input =0;
+		assertEquals(1,input);
+	}
+
+}
