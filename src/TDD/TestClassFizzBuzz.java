602c196d748fbdd65d09311af3828acf8074b723
Rattiyakorn <rattiyakornl@kkumail.com>
2019-10-08 11:02:49 +0700

    I DO Test2

--- src/TDD/TestClassFizzBuzz.java
+++ src/TDD/TestClassFizzBuzz.java
@@ -8,11 +8,14 @@ import org.junit.jupiter.api.Test;
 class TestClassFizzBuzz {
 	FizzBuzz fizzbuzz = new FizzBuzz();
 	String input1 = fizzbuzz.getnumber1(1);
-
+	String input2 = fizzbuzz.getnumber1(2);
+	
 	@Test
 	void test1() {
 		assertEquals("1", input1);
-		
-
-}
+	}
+	@Test
+	void test2() {
+		assertEquals("2", input2);
+	}
 }
