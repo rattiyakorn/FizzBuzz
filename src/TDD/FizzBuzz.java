8eea8e1250ffe40ce5c5d2cb5a7fec0e7d9fb5c0
NIsarat <nnnisa@kkumail.com>
2019-10-08 10:50:52 +0700

    1.1

---
+++ src/TDD/FizzBuzz.java
@@ -0,0 +1,27 @@
+package TDD;
+import java.util.Scanner;
+public class FizzBuzz {
+	Scanner keyboardScanner = new Scanner(System.in);
+	
+	public String getnumber1(int number1) {
+		 if(number1 % 3 == 0) {
+			 return Fizz();
+		 }
+		 else if(number1 % 5 == 0) {
+			 return Buzz();
+		 }
+		 else if((number1%3==0)&(number1%5==0))
+		 {
+			 return Buzz();
+		 }else {
+			 return  Integer.toString(number1);
+		 }
+	}
+	public String Fizz() {
+		return "Fizz";
+	}
+	public String Buzz() {
+		return "Buzz";
+	}
+
+}
--- src/TDD/TestClassFizzBuzz.java
+++ src/TDD/TestClassFizzBuzz.java
@@ -1,15 +1,17 @@
 package TDD;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
+
+
 class TestClassFizzBuzz {
+	FizzBuzz fizzbuzz = new FizzBuzz();
+	String input1 = fizzbuzz.getnumber1(1);
 
 	@Test
-	void input_is_1_expect_1() {
-		int input =0;
-		assertEquals(1,input);
-	}
+	void test1() {
+		assertEquals("1", input1);
 
+}
 }
